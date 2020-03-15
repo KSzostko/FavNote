066e903253c4bd421cd7d6b64917bfa8acbf1756
Kuba Szóstko <kubaszostko@gmail.com>
2020-03-15 17:09:41 +0100

    added Form component and partial styles to it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import List from './components/List/List';
+import Form from './components/Form/Form';
 
 function App() {
     return (
         <>
             <h1>Hello world!</h1>
             <List />
+            <Form />
         </>
     );
 }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Button from '../Button/Button';
+import style from './Form.module.scss';
+
+const Form = () => (
+    <div className={style.wrapper}>
+        <Button className={style.closeButton}></Button>
+        <Header>Add new favourite Twitter account</Header>
+        <form className={style.form}>
+            <p>
+                <label className={style.form__label} htmlFor="title">Twitter Name</label>
+                <input className={style.form__input} type="text" />
+            </p>
+            <p>
+                <label className={style.form__label} htmlFor="link">Twitter Link</label>
+                <input className={style.form__input} type="text" />
+            </p>
+            <p>
+                <label className={style.form__label} htmlFor="image">Image</label>
+                <input className={style.form__input} type="text" />
+            </p>
+            <p>
+                <label className={style.form__label} htmlFor="description">Description</label>
+                <textarea className={style.form__textarea} name="description"></textarea>
+            </p>
+            <Button>add new item</Button>
+        </form>
+    </div>
+);
+
+export default Form;
