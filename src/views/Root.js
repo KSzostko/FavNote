1602ef5c592d17447b7bb20633b038c48199c5b5
Kuba Szóstko <kubaszostko@gmail.com>
2020-03-19 17:43:06 +0100

    created views folder

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import List from './components/List/List';
-import Form from './components/Form/Form';
-import Header from './components/Header/Header';
+import List from '../components/List/List';
+import Form from '../components/Form/Form';
+import Header from '../components/Header/Header';
 
 function App() {
     return (
