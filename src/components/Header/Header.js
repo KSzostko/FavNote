3a7500dacc7b909302eb303af7c70f484a431e46
Kuba Szóstko <kubaszostko@gmail.com>
2020-03-19 11:21:51 +0100

    added new Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Header.module.scss';
+
+const Header = () => (
+    <div className={style.wrapper}>
+        Header
+    </div>
+);
+
+export default Header;
