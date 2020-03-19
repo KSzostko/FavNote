db9417a7cf2aac2458f5dd0581ed2cdd0c30a6f6
Kuba Szóstko <kubaszostko@gmail.com>
2020-03-19 11:30:23 +0100

    added Button to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from '../Button/Button';
 import style from './Header.module.scss';
 import logo from '../../assets/img/logo.svg';
 
 const Header = () => (
     <div className={style.wrapper}>
         <img src={logo} alt="Fav Note" />
+        <Button>new item</Button>
     </div>
 );
 
