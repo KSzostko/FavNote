4eedc6a92d5fcbe2fd7dc3c43f83c21e3782f0cb
Kuba Szóstko <kubaszostko@gmail.com>
2020-03-20 12:01:01 +0100

    changed Form to class component and addent method for handling radio

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,38 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Title from '../Title/Title';
 import Button from '../Button/Button';
 import FormRadio from './FormRadio/FormRadio';
 import style from './Form.module.scss';
 
-const Form = () => (
-    <div className={style.wrapper}>
-        <button className={style.closeButton}></button>
-        <Title>Add new favourite Twitter account</Title>
-        <form className={style.form}>
-            <div className={style.form__radioWrapper}>
-                <FormRadio>Twitter</FormRadio>
-                <FormRadio>Article</FormRadio>
-                <FormRadio>Note</FormRadio>
+const types = {
+    twitter: 'twitter',
+    article: 'article',
+    note: 'note'
+};
+
+class Form extends Component {
+    state = {
+        checked: types.twitter
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            checked: e.target.id
+        });
+    }
+
+    render() {
+        const { checked } = this.state;
+
+        return (
+            <div className={style.wrapper}>
+                <button className={style.closeButton}></button>
+                <Title>Add new favourite Twitter account</Title>
+                <form className={style.form}>
+                    <div className={style.form__radioWrapper}>
+                        <FormRadio
+                            id={types.twitter}
+                            checked={checked === types.twitter}
+                            change={this.handleChange}
+                        >
+                            Twitter
+                        </FormRadio>
+                        <FormRadio
+                            id={types.article}
+                            checked={checked === types.article}
+                            change={this.handleChange}
+                        >
+                            Article
+                        </FormRadio>
+                        <FormRadio
+                            id={types.note}
+                            checked={checked === types.note}
+                            change={this.handleChange}
+                        >
+                            Note
+                        </FormRadio>
+                    </div>
+                    <p className={style.form__inputWrapper}>
+                        <label className={style.form__label} htmlFor="title">Twitter Name</label>
+                        <input className={style.form__input} type="text" />
+                    </p>
+                    <p className={style.form__inputWrapper}>
+                        <label className={style.form__label} htmlFor="link">Twitter Link</label>
+                        <input className={style.form__input} type="text" />
+                    </p>
+                    <p className={style.form__inputWrapper}>
+                        <label className={style.form__label} htmlFor="image">Image</label>
+                        <input className={style.form__input} type="text" />
+                    </p>
+                    <p className={style.form__inputWrapper}>
+                        <label className={style.form__label} htmlFor="description">Description</label>
+                        <textarea className={style.form__textarea} name="description" rows="10"></textarea>
+                    </p>
+                    <Button>add new item</Button>
+                </form>
             </div>
-            <p className={style.form__inputWrapper}>
-                <label className={style.form__label} htmlFor="title">Twitter Name</label>
-                <input className={style.form__input} type="text" />
-            </p>
-            <p className={style.form__inputWrapper}>
-                <label className={style.form__label} htmlFor="link">Twitter Link</label>
-                <input className={style.form__input} type="text" />
-            </p>
-            <p className={style.form__inputWrapper}>
-                <label className={style.form__label} htmlFor="image">Image</label>
-                <input className={style.form__input} type="text" />
-            </p>
-            <p className={style.form__inputWrapper}>
-                <label className={style.form__label} htmlFor="description">Description</label>
-                <textarea className={style.form__textarea} name="description" rows="10"></textarea>
-            </p>
-            <Button>add new item</Button>
-        </form>
-    </div>
-);
+        );
+    }
+}
 
 export default Form;
--- src/components/Form/FormRadio/FormRadio.js
+++ src/components/Form/FormRadio/FormRadio.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import style from './FormRadio.module.scss';
 
-const FormRadio = ({ children }) => (
+const FormRadio = ({ id, children, checked, change }) => (
     <label className={style.radio}>
         <input
+            id={id}
             className={style.input}
             type="radio"
+            checked={checked}
+            onChange={change}
         />
         <div className={style.radioCheck}></div>
         {children}
