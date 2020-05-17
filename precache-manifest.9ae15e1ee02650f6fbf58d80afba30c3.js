55cc14b91722fdea52e06d7b21fa4f3f18180a83
Kuba Szóstko <kubaszostko@gmail.com>
2020-05-17 19:59:39 +0200

    Updates

---
+++ precache-manifest.9ae15e1ee02650f6fbf58d80afba30c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718d0b5424653b83ac654fb2836f8771",
+    "url": "/FavNote/index.html"
+  },
+  {
+    "revision": "65298f838ae3a8a81e5b",
+    "url": "/FavNote/static/css/main.94e8700c.chunk.css"
+  },
+  {
+    "revision": "07855c54a10b7c2faa13",
+    "url": "/FavNote/static/js/2.dda47692.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/FavNote/static/js/2.dda47692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65298f838ae3a8a81e5b",
+    "url": "/FavNote/static/js/main.9e20b216.chunk.js"
+  },
+  {
+    "revision": "24ee66fe7333d247a2bf",
+    "url": "/FavNote/static/js/runtime-main.4dc7847a.js"
+  },
+  {
+    "revision": "d650c88b859ee642dfde895fdc682827",
+    "url": "/FavNote/static/media/logo.d650c88b.svg"
+  }
+]);
